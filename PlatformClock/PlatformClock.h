d72e028fadef5668ae3873f6f3b76b7252dd3024
Felix Wong <Felix Wong>
2017-01-09 16:11:17 -0500

    Added clock defines.

---
+++ PlatformClock/PlatformClock.h
@@ -0,0 +1,14 @@
+/*
+ * PlatformClock.h
+ *
+ * Created: 2016-09-11 3:58:23 PM
+ *  Author: Felix
+ */ 
+
+#ifndef PLATFORMCLOCK_H_
+#define PLATFORMCLOCK_H_
+
+#define F_CPU 16000000 // Define CPU clock speed to be 16MHz
+
+
+#endif /* PLATFORMCLOCK_H_ */
